23595bc9db82c8106dc0c25f3da0bac792ebc5d6
Jesse Pasos <jessejayp@gmail.com>
2015-04-07 20:25:42 -0700

    Update submitproperty.blade.php

--- resources/views/pages/submitproperty.blade.php
+++ resources/views/pages/submitproperty.blade.php
@@ -4,33 +4,32 @@
 
 @section('body')
 
-    <?php echo $agent_id; ?>
-
-            <div id='contentnav'>Submit New Property</div>
-            <div id='contentblock'>
-                <p>
-                    <form method='post' action='submit'>
-                        <input type='hidden' name='_token' value='<?php echo csrf_token(); ?>'>
-                        <input type='hidden' name='agent_id' value='<?php echo $agent_id; ?>'>
-                        <p>ID: <br/>
-                        <input type='number' name='sub_ID' id='formstyle'/></p>
-                        <p>Address: <br/>
-                        <input type='text' name='sub_add' id='formstyle'/> </p>
-                        <p>MLS: <br/>
-                        <input type='text' name='sub_mls' id='formstyle'/> </p>
-                        <p>Square Footage: <br/>
-                        <input type='text' name='sub_sqr' id='formstyle'/> </p>
-                        <p>Sub-area: <br/>
-                        <select name='sub_dist' id='formstyle'>
-                            <option value='BW'>Big White</option>
-                            <option value='BL'>Black Mountain</option>
-                            <option value='BE'>Beaverdell-Carmi</option>
-                        </select></p>
-                        <p><input type='submit' name='submit_prop' value='Submit' id='formstyle'/></p>
-                    </form>
-                </p>
-            </div>
-
-
-
+    <div class='contentheader'>Submit New Property</div>
+    <div class='contentblock'>
+        <p>
+            <form method='post' action='submit'>
+                <input type='hidden' name='_token' value='<?php echo csrf_token(); ?>'>
+                <input type='hidden' name='agent_id' value='<?php echo $agent_id; ?>'>
+                <div class="formcontainer">Address: <br/>
+                    <input type='text' name='sub_add' id='formstyle'/>
+                </div>
+                <div class="formcontainer">MLS: <br/>
+                    <input type='text' name='sub_mls' id='formstyle'/>
+                </div>
+                <div class="formcontainer">Square Footage: <br/>
+                    <input type='text' name='sub_sqr' id='formstyle'/>
+                </div>
+                <div class="formcontainer">Sub-area: <br/>
+                    <select name='sub_dist' id='formstyle'>
+                        <option value='LCU'>Winfield, Ellison, Lake Country, Quail</option>
+                        <option value='KNG'>Dilworth, Glenmore, North Glenmore, Kelowna North</option>
+                        <option value='KSM'>Sprintfield, Spall, Mission, Southeast Kelowna, Kelowna South</option>
+                        <option value='WK'>Westside</option>
+                        <option value='RNS'>Rutland, Black Mountain, Joe Rich</option>
+                    </select>
+                </div>
+                <div style='clear:both; margin: 10px;'><input type='submit' name='submit_prop' value='Submit' id='formstyle'/></div>
+            </form>
+        </p>
+    </div>
 @stop

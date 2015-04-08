bc759f88b9e1c044285c15bbc43bf0d15a99ab4a
Jesse Pasos <jessejayp@gmail.com>
2015-04-07 20:24:00 -0700

    Update mylistings.blade.php

--- resources/views/pages/mylistings.blade.php
+++ resources/views/pages/mylistings.blade.php
@@ -4,77 +4,81 @@
 
 @section('body')
     <?php
-        $agent_id;
-        $status;
+    $agent_id;
+    $status;
 
-        $listings = DB::select('select * from listings L, properties P where L.property_id = P.id and L.agent_id = ?', [$agent_id]);
-        $contentblock = "<div id='contentnav'>Your Properties</div><div id='contentblock'>";
-        $contentblock .= "<table id='tourTable'><thead><tr><th>MLS</th><th>Address</th><th>District</th><th>Status</th><th>Modify</th><th>Confirm Property</th></tr></thead><tbody>";
+    $listings = DB::select('select * from listings L, properties P where L.property_id = P.id and L.agent_id = ?', [$agent_id]);
+    echo "<div class='contentheader'>Your Properties</div>
+                <div class='contentblock'>";
 
-        foreach ($listings as $listings)
+    foreach ($listings as $listings)
+    {
+        echo "<table class='datatable'>";
+        echo "<tr><th>MLS:</th><td>".$listings->mls."</td></tr>
+                <tr><th>Address:</th><td>".$listings->address."</td></tr>
+                <tr><th>Area:</th><td>".$listings->district_code."</td></tr>";
+
+        $status = $listings->status;
+        switch($status)
         {
-            $contentblock .= "<tr>";
-            $contentblock .= "<td>".$listings->mls."</td>";
-            $contentblock .= "<td>".$listings->address."</td>";
-            $contentblock .= "<td>".$listings->district_code."</td>";
-
-            $status = $listings->status;
-            switch($status)
-            {
-                case 'S':
-                    $status = "Submitted";
-                    break;
-                case 'C':
-                    $status = "Confirmed";
-                    break;
-                case 'R':
-                    $status = "Removed";
-                    break;
-                case 'T':
-                    $status = "On Tour";
-                    break;
-            }
-            if($listings->status == 'S')
-            {
-                $edit = "<form method='post' action='viewlisting'>
-                            <input type='hidden' name='_token' value='".csrf_token()."'>
-                            <input type='hidden' name='prop_id' value='".$listings->property_id."'>
-                            <input type='submit' name='p_edit' value='Edit'/></form>";
-                $confirm = "<form method='post' action='confirm'>
-                            <input type='hidden' name='_token' value='".csrf_token()."'>
-                            <input type='hidden' name='prop_id' value='".$listings->property_id."'>
-                            <input type='submit' name='p_conf' value='Confirm'/></form>";
-            }
-            else
-            {
-                $edit = "";
-                $confirm = "";
-            }
-            $contentblock .= "<td>".$status."</td>";
-            $contentblock .= "<td>".$edit."</td>";
-            $contentblock .= "<td>".$confirm."</td>";
-            $contentblock .= "</tr>";
+            case 'S':
+                $status = "Submitted";
+                break;
+            case 'C':
+                $status = "Confirmed";
+                break;
+            case 'R':
+                $status = "Removed";
+                break;
+            case 'T':
+                $status = "On Tour";
+                break;
         }
-        $contentblock .= "</tbody></table>";
-        $contentblock .= "<p>Note: by confirming a property, you will no longer be able to make any changes.</p>";
-        $contentblock .= "<p>If any changes need to be made to a confirmed property, please contact the office administrator.</p></div>";
-
-        $searchblock = "<div id='contentnav'>Search</div><div id='contentblock'>";
-        $searchblock .= "<p>MLS: <form method='post' action='search_mls'>
+        if($listings->status == 'S')
+        {
+            $edit = "<form method='post' action='view_listing'>
                             <input type='hidden' name='_token' value='".csrf_token()."'>
-                            <p><input type='text' name='s_mls'/></p>
-                            <p><input type='submit' name='src' value='Search'/></p>
-                            </form></p>";
-        $searchblock .= "<p>Address: <form method='post' action='search_add'>
+                            <input type='hidden' name='prop_id' value='".$listings->property_id."'>
+                            <input type='submit' name='p_edit' value='Edit' style='width: 80px;'/></form>";
+            $confirm = "<form method='post' action='confirm'>
                             <input type='hidden' name='_token' value='".csrf_token()."'>
-                            <p><input type='text' name='s_add'/></p>
-                            <p><input type='submit' name='src' value='Search'/></p>
-                            </form></p>";
-        $searchblock .= "</div>";
+                            <input type='hidden' name='prop_id' value='".$listings->property_id."'>
+                            <input type='submit' name='p_conf' value='Confirm' style='width: 80px;'/></form>";
+        }
+        else
+        {
+            $edit = "";
+            $confirm = "";
+        }
+        echo "<tr><th>Status:</th><td>".$status."</td></tr>
+                <tr><td>".$edit."</td><td>".$confirm."</td></tr>";
+        echo "</table>";
+    }
 
+    echo "
+                <div style='clear:both;'>Note: by confirming a property, you will no longer be able to make any changes.<br/>
+                If any changes need to be made to a confirmed property, please contact the office administrator.</div>
+            </div>";
+
+    echo "<div class='contentheader'>Search</div>
+                        <div class='contentblock'>
+                            <div class='formcontainer'>
+                                MLS:
+                                <form method='post' action='search_mls'>
+                                    <input type='hidden' name='_token' value='".csrf_token()."'>
+                                    <p><input type='text' name='s_mls'/></p>
+                                    <p><input type='submit' name='src' value='Search'/></p>
+                                </form>
+                                </div>
+                            <div class='formcontainer'>
+                                Address:
+                                <form method='post' action='search_add'>
+                                    <input type='hidden' name='_token' value='".csrf_token()."'>
+                                    <p><input type='text' name='s_add'/></p>
+                                    <p><input type='submit' name='src' value='Search'/></p>
+                                </form>
+                            </div>
+                        </div>";
     ?>
-
-    <?php echo "$contentblock"; ?>
-    <?php echo "$searchblock"; ?>
 
 @stop

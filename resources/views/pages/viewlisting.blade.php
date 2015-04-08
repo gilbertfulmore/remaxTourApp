6db778217c6f8fef0c68b81294aab87f7285ffe3
Jesse Pasos <jessejayp@gmail.com>
2015-04-07 20:26:27 -0700

    Update viewlisting.blade.php

--- resources/views/pages/viewlisting.blade.php
+++ resources/views/pages/viewlisting.blade.php
@@ -4,43 +4,45 @@
 
 @section('body')
     <?php
-        $prop_id = array_flatten($prop_id);
-        $prop_id = $prop_id[0];
 
-        $listings = DB::select('select * from listings L, properties P where L.property_id = P.id and L.property_id = ?', [$prop_id]);
-        $contentblock = "<div id='contentnav'>Edit Property</div><div id='contentblock'>";
-        $contentblock .= "<form method='post' action='editlisting'>
-                            <input type='hidden' name='_token' value='".csrf_token()."'>
-                            <input type='hidden' name='p_id' value='".$prop_id."'>";
-        $contentblock .= "<table id='tourTable'><thead><tr><th>MLS</th><th>Address</th><th>Area (sq feet)</th><th>Sub Area</th></tr></thead><tbody>";
+    $listings = DB::select('select * from listings L, properties P where L.property_id = P.id and L.property_id = ?', [$prop_id]);
 
-        foreach ($listings as $listings)
-        {
-            $contentblock .= "<tr>";
-            $contentblock .= "<td>".$listings->mls."</td>";
-            $contentblock .= "<td>".$listings->address."</td>";
-            $contentblock .= "<td>".$listings->sq_feet."</td>";
-            $contentblock .= "<td>".$listings->district_code."</td>";
-            $contentblock .= "</tr>";
-        }
-        $contentblock .= "<tr>";
-        $contentblock .= "<td><input type='text' name='p_mls' id='formstyle' value='".$listings->mls."'/></td>";
-        $contentblock .= "<td><input type='text' name='p_add' id='formstyle' value='".$listings->address."'/></td>";
-        $contentblock .= "<td><input type='number' name='p_sq' id='formstyle' value='".$listings->sq_feet."'/></td>";
-        $contentblock .= "<td><select name='p_dist' id='formstyle'>
-                             <option value='BW'>Big White</option>
-                             <option value='BL'>Black Mountain</option>
-                             <option value='BE'>Beaverdell-Carmi</option>
-                          </select></td>";
-        $contentblock .= "</tr>";
+    echo "<div class='contentheader'>View Property</div><div class='contentblock'>";
 
-        $contentblock .= "</tbody></table>";
-        $contentblock .= "<p><input type='submit' name='p_save' value='Save Changes' id='formstyle'/></p>";
-        $contentblock .= "</form></div>";
+    foreach ($listings as $listings) {
+        echo "<div class='datacontainer'>
+                        <div class='datahead'>MLS:</div>
+                        <div class='databody'>" . $listings->mls . "</div>
+                        <div class='datahead'>Address:</div>
+                        <div class='databody'>" . $listings->address . "</div>
+                        <div class='datahead'>Square Ft:</div>
+                        <div class='databody'>" . $listings->sq_feet . "</div>
+                        <div class='datahead'>Area:</div>
+                        <div class='databody'>" . $listings->district_code . "</div>
+                    </div>";
+    }
+    echo "</div>";
 
+    echo "<div class='contentheader'>Edit Property</div>
+    <div class='contentblock'>
+        <form method='POST' action='edit_listing'>
+            <input type='hidden' name='_token' value='".csrf_token()."'>
+            <input type='hidden' name='prop_id' value='".$prop_id."'>
+            <label>MLS: <input type='text' name='prop_mls' value='".$listings->mls."'></label><br/>
+            <label>Address: <input type='text' name='prop_add' value='".$listings->address."'></label><br/>
+            <label>Square feet: <input type='number' name='prop_sq' value='".$listings->sq_feet."'></label><br/>
+            <label>Sub-area: <select name='prop_dist'>
+                <option value='LCU'>Winfield, Ellison, Lake Country, Quail</option>
+                <option value='KNG'>Dilworth, Glenmore, North Glenmore, Kelowna North</option>
+                <option value='KSM'>Sprintfield, Spall, Mission, Southeast Kelowna, Kelowna South</option>
+                <option value='WK'>Westside</option>
+                <option value='RNS'>Rutland, Black Mountain, Joe Rich</option>
+            </select></label>
 
+            <p><input type='submit' name='p_save' value='Save Changes' id='formstyle'/></p>
+        </form>
+    </div>";
     ?>
 
-    <?php echo "$contentblock"; ?>
 
 @stop

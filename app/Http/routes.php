5903c62e460c5d2e2fc98e57ef5dd93b69296386
fordesta <fordesta@users.noreply.github.com>
2015-04-07 21:00:32 -0700

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@ Route::group(['prefix' => 'admin'], function() {
 
     Route::get('agents', 'AdminController@agents');
 
-    Route::get('register', 'AdminController@register');
-
-    Route::post('register', 'EmailController@accountCreation');
+    Route::any('register', 'AdminController@register');
 
     Route::any('edituser', 'AdminController@edituser');
 
@@ -21,7 +19,19 @@ Route::group(['prefix' => 'admin'], function() {
 
     Route::any('removetour', 'AdminController@removetour');
 
-    Route::any('email_settings', 'AdminController@email_settings');
+    Route::any('markremove', 'AdminController@markremove');
+
+    Route::any('marksubmit', 'AdminController@marksubmit');
+
+    Route::any('newtour', 'AdminController@newtour');
+
+    Route::any('organize', 'AdminController@organize');
+
+    Route::any('changetour', 'AdminController@changetour');
+
+    Route::any('handlechange', 'AdminController@handlechange');
+
+    Route::any('reorganize', 'AdminController@reorganize');
 });
 
 Route::get('/', 'PagesController@index');
@@ -32,7 +42,7 @@ Route::get('help', 'PagesController@help');
 
 Route::get('tours', 'PagesController@tours');
 
-Route::any('mylistings', 'PagesController@myListings');
+Route::any('listings', 'PagesController@listings');
 
 Route::post('confirm', 'PagesController@postconfirm');
 
@@ -40,9 +50,7 @@ Route::post('search_mls', 'PagesController@postsearch_mls');
 
 Route::post('search_add', 'PagesController@postsearch_add');
 
-Route::get('submitproperty', 'PagesController@submitProperty');
-
-Route::post('submitproperty', 'EmailController@submitProperty');
+Route::any('submit', 'PagesController@submit');
 
 Route::any('toursummary', 'PagesController@tourSummary');
 

2a3f6d741e14861462e48361fe1ba31ff67b6955
Jesse Pasos <jessejayp@gmail.com>
2015-04-10 13:09:25 -0700

    Update organize.blade.php

--- resources/views/admin/organize.blade.php
+++ resources/views/admin/organize.blade.php
@@ -7,7 +7,7 @@
     <?php
     $prop_arr = array();
     $count = 0;
-    $listings = DB::select('select * from listings where status = "t" and tour_id > 1 order by rank');
+    $listings = DB::select('select * from listings where status = "T" and tour_id > 1 order by rank');
     $contentblock = "";
     $mcount = count($listings);
     $mcount++;
@@ -21,7 +21,7 @@
                 . "<table id='tourTable'><thead><tr><th>Agent ID</th><th>MLS</th><th>Property ID</th><th>Status</th><th>Rank</th><th>Modify</th></tr></thead><tbody>";
         foreach ($listings as $listing) {
             $p_stat = $listing->status;
-            if($p_stat == 't'){
+            if($p_stat == 'T'){
                 $p_edit = "<form method='post' action='reorganize'>"
                         ."<input type='hidden' name='_token' value='".csrf_token()."'>"
                         ."<input type='hidden' name='property_id' value='".$listing->property_id."'>"
@@ -38,7 +38,7 @@
             }
             switch($p_stat)
             {
-                case 't':
+                case 'T':
                     $p_stat = "On Tour";
                     break;
             }
